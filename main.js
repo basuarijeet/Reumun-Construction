4ea34fab170a501ab039b2c09f7b896ba38176b3
Arijeet Basu <117835217+basuarijeet@users.noreply.github.com>
2023-10-21 19:41:22 +0530

    Commit1

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const progress = document.querySelector('.progress');
+    const percentage = document.querySelector('.progress span');
+
+    let per = 0;
+    function progressLoad() {
+        if (per < 80) {
+            progress.style.width = `${per}%`;
+            percentage.innerHTML = `${per}%`;
+            per++;
+        }
+    }
+
+    setInterval(progressLoad, 90);
+});
